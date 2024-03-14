c11fe8318faf633f3c2d78a29f6d40301a47be6f
Alexander Kruschewsky <kruschewsky@embedded.rwth-aachen.de>
2024-03-14 20:42:34 +0100

    Fixed Type-Issue, when Hickle does not properly interpret the correct type of weights.

--- NDAS/ndas/dataimputationalgorithms/pandas_interpolation.py
+++ NDAS/ndas/dataimputationalgorithms/pandas_interpolation.py
@@ -62,8 +62,13 @@ class PandasInterpolation:
             found_dt = physiologicallimits.get_physical_dt(col)
             if found_dt and found_dt.id in pre_trained_columns and isinstance(transformed_other_series_dict[found_dt.id], np.ndarray):
                 weights = hkl.load(file_path+found_dt.id+"_weights.hkl")
-                weighted_transformed_column = transformed_other_series_dict[found_dt.id] * np.squeeze(weights[0])
-                interpolated_other_series.append(np.sum(weighted_transformed_column, axis=1)+np.squeeze(weights[1]))
+                if isinstance(weights, dict):
+                    weight_keys = list(weights.keys())
+                    weighted_transformed_column = transformed_other_series_dict[found_dt.id] * np.squeeze(weights[weight_keys[0]][0])
+                    interpolated_other_series.append(np.sum(weighted_transformed_column, axis=1)+np.squeeze(weights[weight_keys[1]][0]))
+                else:
+                    weighted_transformed_column = transformed_other_series_dict[found_dt.id] * np.squeeze(weights[0])
+                    interpolated_other_series.append(np.sum(weighted_transformed_column, axis=1)+np.squeeze(weights[1]))
                 mask_min.append(found_dt.low)
                 mask_max.append(found_dt.high)
             else:
