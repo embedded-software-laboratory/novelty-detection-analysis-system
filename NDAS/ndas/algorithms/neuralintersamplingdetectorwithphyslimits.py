0acc85fa4fffec6e571cbb779ce728badf90dfe3
Alexander Kruschewsky <kruschewsky@embedded.rwth-aachen.de>
2024-03-14 20:51:55 +0100

    Include Nan-contingency measures from with Approx method

--- NDAS/ndas/algorithms/neuralintersamplingdetectorwithphyslimits.py
+++ NDAS/ndas/algorithms/neuralintersamplingdetectorwithphyslimits.py
@@ -75,13 +75,18 @@ class NeuralInterSamplingDetectorWithPhysicalLimits(BaseDetector):
             self.signal_percentage(int(current_status) % 100)
             novelty_data = {}
             data = clipped_dataset[[c]]
-            if np.count_nonzero(~np.isnan(data.values)) > 2:
+            if np.count_nonzero(~np.isnan(data.values)) > 10:
                 imputed_data = imputed_dataset[[c]].iloc[data.index]
                 data_diff = (data - imputed_data).abs()
                 sorted_data_diff = np.sort(data_diff.values, axis=None)
                 sorted_data_diff = sorted_data_diff[~np.isnan(sorted_data_diff)]
-                knee_result = KneeLocator(range(len(sorted_data_diff)), sorted_data_diff, S=self.s_value, curve='convex', direction='increasing')
-                threshold_value = knee_result.knee_y
+                knee_result = None
+                if len(sorted_data_diff) > 2:
+                    knee_result = KneeLocator(range(len(sorted_data_diff)), sorted_data_diff, S=self.s_value, curve='convex', direction='increasing')
+                if knee_result:
+                    threshold_value = knee_result.knee_y
+                if not threshold_value:
+                    threshold_value = sorted_data_diff[-1]
                 for index, row in data_diff.iterrows():
                     if row[c] > threshold_value:
                         novelty_data[datasets[time_column].values[index]] = 1
